7e5cc95fd7172e21675bf86f2b5b7f755010c672
Bang Tedy <tedysuwega@gmail.com>
2019-02-13 13:55:50 +0700

    try method (succes show image)

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ import  modules
 
 if __name__ == '__main__':
     print('hello this is where image will show')
-    img = modules.load_images('D:\Another\Image\image\image_1.jpg')
+    img = modules.load_images('..\image_1.jpg')
     modules.show_image(img)
+
--- modules.py
+++ modules.py
@@ -1,9 +1,5 @@
 import cv2
 
-def loadImage(image):
-    result = cv2.imread(image)
-    return result
-
 def load_images(loc):
     return cv2.imread(loc)
 
@@ -11,11 +7,15 @@ def rgb2gray(img):
     return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 def show_image(img):
-    cv2.imshow(img,0)
+    cv2.imshow('', img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-# a = loadImage()
-a = cv2.imread('D:\Another\Image\image\image_1.jpg')
-show_image(a)
-# print(a)
+
+loc = "D:\Another\Image\image_\image_1.jpg"
+img = cv2.imread(loc)
+print(img)
+cv2.imshow('',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+# img = load_images(loc)
