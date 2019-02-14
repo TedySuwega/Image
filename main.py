432d48079e7907faaf8b10d57971e2ee3a07d94a
Bang Tedy <tedysuwega@gmail.com>
2019-02-14 17:35:38 +0700

    update histogram

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import  modules
 
 if __name__ == '__main__':
-    print('hello this is where image will show')
     a = modules.load_images("D:\Another\Image\image_\image_1.jpg")
-    # modules.show_image(a)
-    print(a)
     modules.show_image(a)
+    hist = modules.calc_histogram(a)
+    print(hist)
+    grey = modules.rgb2gray(a)
+    modules.show_image(grey)
+    bw = modules.gray2bw(grey)
+    modules.show_image(bw)
+
+    import numpy as np
+    from matplotlib import pyplot as plt
+    plt.hist(a.ravel(), 256, [0, 256]);
+    plt.show()
--- modules.py
+++ modules.py
@@ -10,3 +10,17 @@ def show_image(img):
     cv2.imshow('', img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+def calc_histogram(img):
+    hist = cv2.calcHist([img], [0], None, [256], [0, 256])
+    return (hist)
+
+def gray2bw(img):
+    """
+    Convert grayscaled image into black & white image
+
+    :param img: Grayscaled image
+    :return: Black & white image
+    """
+    # return cv2.threshold(img, 75, 255, cv2.THRESH_TOZERO)[1]
+    return cv2.threshold(img, 100, 255, cv2.THRESH_BINARY)[1]
